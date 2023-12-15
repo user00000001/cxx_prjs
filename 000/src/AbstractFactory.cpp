d424da3b796a1da149be30aa24746815dc0f8b3b
暾暾 <631078133@qq.com>
2023-12-15 15:45:04 +0800

    normal update.
    Signed-off-by: 暾暾 <631078133@qq.com>

---
+++ 000/src/AbstractFactory.cpp
@@ -0,0 +1,17 @@
+#include "include/AbstractFactory.hpp"
+#include "include/Product.hpp"
+#include <iostream>
+using namespace std;
+
+AbstractProductA * ConcreteFactory1::CreateProductA() {
+  return new ProductA1();
+}
+AbstractProductB * ConcreteFactory1::CreateProductB() {
+  return new ProductB1();
+}
+AbstractProductA * ConcreteFactory2::CreateProductA() {
+  return new ProductA2();
+}
+AbstractProductB * ConcreteFactory2::CreateProductB() {
+  return new ProductB2();
+}
